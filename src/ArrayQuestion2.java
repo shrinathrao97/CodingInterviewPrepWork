dc5dd7c30bf71aed75c7828fc40d8e1756c02e08
Shrinath Rao <shrinathrao97@gmail.com>
2020-05-05 13:02:19 -0500

    Assignment 2

--- src/ArrayQuestion2.java
+++ src/ArrayQuestion2.java
@@ -1,9 +1,41 @@
-
+import java.util.Arrays;
 public class ArrayQuestion2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//
+	void assignment2(int inputArray[]){
+		
+		Arrays.sort(inputArray);
+		
+		for(int i = 0; i < inputArray.length; i++){
+			
+			for(int j = i + 1; j < inputArray.length; j++){
+				
+				if (inputArray[j] == inputArray[i]){
+					
+					System.out.print(inputArray[i] + " has a duplicate\n");
+					
+				}	
+			}
+			
+			
+		}
+		
+		System.out.print("Done\n");
 	}
+	
+	
+	public static void main(String[] args) {
+		//We don't have particularly useful information of the values in the arrays
+		//The values could be anything, so I think we'd need two for loops...
+		//Here's an array of 10 elements I guess
+		
+		int array[] = new int[]{1,2,3,4,5,6,7,8,9,10};
+		ArrayQuestion2 test = new ArrayQuestion2();
+		test.assignment2(array);
+		
+	}
+	
+	
 
 }
